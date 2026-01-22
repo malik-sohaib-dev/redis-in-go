6b3b6c764b7da479dadb0658bf8e7c12d506725a
Malik <asohaib575@gmail.com>
2026-01-22 13:39:36 +0500

    GO Routines for concurrency

--- app/main.go
+++ app/main.go
@@ -4,11 +4,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"sync"
 )
-
-// // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-// var _ = net.Listen
-// var _ = os.Exit
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
@@ -19,6 +16,8 @@ func main() {
 		os.Exit(1)
 	}
 
+	var wg sync.WaitGroup
+
 	for {
 		conn, err := l.Accept()
 		fmt.Println("Connected!")
@@ -26,16 +25,68 @@ func main() {
 			fmt.Println("Error accepting connection: ", err.Error())
 			continue
 		}
-		for {
-			b := make([]byte, 1024)
-			n, err := conn.Read(b)
-			if err != nil {
-				break
-			}
-			fmt.Println(n)
-			fmt.Println("Incoming: ", string(b[:n]))
-			conn.Write([]byte("+PONG\r\n"))
-		}
-		conn.Close()
+		// This method came in Go 1.25+. Before we had to manually do wg.Add(1) and wg.Done()
+		wg.Go(func() {
+			pingPong(conn)
+		})
 	}
 }
+
+func pingPong(conn net.Conn) {
+	for {
+		b := make([]byte, 1024)
+		n, err := conn.Read(b)
+		if err != nil {
+			break
+		}
+		fmt.Println(n)
+		fmt.Println("Incoming: ", string(b[:n]))
+		conn.Write([]byte("+PONG\r\n"))
+	}
+	fmt.Println("Closing Connection!")
+	conn.Close()
+}
+
+// func main() {
+// 	fmt.Println("Starting here!")
+
+// 	// Start TCP connection
+// 	l, err := net.Listen("tcp", "0.0.0.0:6379")
+// 	if err != nil {
+// 		fmt.Println("Failed to bind to port 6397 due to error: ", err.Error())
+// 		os.Exit(1)
+// 	}
+
+// 	var wg sync.WaitGroup
+// 	for {
+// 		// Make a new connection
+// 		conn, err := l.Accept()
+// 		fmt.Println("Connected!")
+// 		if err != nil {
+// 			fmt.Println("Error accepting connection: ", err.Error())
+// 			continue
+// 		}
+
+// 		// Older method
+// 		wg.Add(1)
+// 		go func() {
+// 			defer wg.Done()
+// 			for {
+// 				// Initialize data buffer
+// 				b := make([]byte, 1024)
+
+// 				n, err := conn.Read(b)
+// 				if err != nil {
+// 					fmt.Println("Failed to Read due to error: ", err.Error())
+// 					break
+// 				}
+
+// 				fmt.Println("Incoming: ", string(b[:n]))
+// 				conn.Write([]byte("+PONG\r\n"))
+// 			}
+// 			// Disconnect
+// 			fmt.Println("Closing Connection!")
+// 			conn.Close()
+// 		}()
+// 	}
+// }
